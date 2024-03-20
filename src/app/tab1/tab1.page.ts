5eb7551c5b939a9cd7646871ec2eb38062877f40
ShehryarSaleem <Shehryarhashmi007@gmail.com>
2024-03-21 01:12:42 +0500

    Logout Functionality

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { HttpCallService } from '../services/auth/http-call.service';
 import { Characters } from './CharacterModal';
 import { NavigationExtras } from '@angular/router';
-import { NavController } from '@ionic/angular';
+import { ActionSheetController, NavController } from '@ionic/angular';
 import { UtilsService } from '../services/utils/utils.service';
 
 @Component({
@@ -12,7 +12,7 @@ import { UtilsService } from '../services/utils/utils.service';
 })
 export class Tab1Page {
   charactersList:Characters[]=[];
-  constructor(private httpCall:HttpCallService, private navCtrl:NavController,public utils:UtilsService) {}
+  constructor(private httpCall:HttpCallService, private navCtrl:NavController,public utils:UtilsService,private actionSheetCtrl:ActionSheetController) {}
 
   ngOnInit() {
     this.httpCall.getList().then((res:any)=>{
@@ -26,6 +26,30 @@ export class Tab1Page {
       }
     };
     this.navCtrl.navigateForward(['tabs/tab1/details'],navigationExtras);
+  }
+  async logout(){
+
+    const actionSheet = await this.actionSheetCtrl.create({
+      buttons: [
+        {
+          text: 'Logout',
+          role: 'destructive',
+          handler: () => {
+            localStorage.clear();
+            this.navCtrl.navigateRoot('login');
+          }
+        },
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          data: {
+            action: 'cancel',
+          },
+        },
+      ],
+    });
+
+    await actionSheet.present();
   }
 
 }
